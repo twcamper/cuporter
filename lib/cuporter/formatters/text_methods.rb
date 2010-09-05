52b4aab491e33516ab25c7ea926e751c23f8bf0b
Tim Camper <twcamper@thoughtworks.com>
2010-09-05 17:46:05 -0500

    simplified text write_node()

--- lib/cuporter/formatters/text_methods.rb
+++ lib/cuporter/formatters/text_methods.rb
@@ -13,17 +13,13 @@ module Cuporter
       def write_node(node, tab_stops)
         @output.puts "#{self.class::TAB * tab_stops}#{node.name}"
         node.children.each do |child|
-          @output.puts "#{self.class::TAB + (self.class::TAB * tab_stops)}#{child.name}"
-          child.children.each do |grand_child|
-            if grand_child.has_children?
-              write_node(grand_child, tab_stops + 2)
-            else
-              @output.puts "#{self.class::TAB * tab_stops}#{self.class::TAB * 2}#{grand_child.name}"
-            end
+          if child.has_children?
+            write_node(child, tab_stops + 1)
+          else
+            @output.puts "#{self.class::TAB * tab_stops}#{self.class::TAB }#{child.name}"
           end
         end
       end
-    
     end
   end
 end
