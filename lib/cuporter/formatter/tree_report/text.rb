19478eecf0ff520e6243cd5bfdd1b5be87ff4193
Tim Camper <twcamper@thoughtworks.com>
2011-04-03 11:01:19 -0400

    basic text tree report

--- lib/cuporter/formatter/tree_report/text.rb
+++ lib/cuporter/formatter/tree_report/text.rb
@@ -6,9 +6,19 @@ module Cuporter
         
         COL_WIDTH = 6
         MARGIN    = 4
-        include TextMethods
         include PrettyTextMethods
         include Cuporter::Formatter::NameReport::TextNodeWriter
+
+        def write_node(node, tab_stops)
+          @output.puts line(node.number, "#{tab * tab_stops}", "[#{node.total}] #{node.name}")
+          node.children.each do |child|
+            if child.has_children? and child.file.nil?
+              write_node(child, tab_stops + 1)
+            else
+              @output.puts line(child.number, "#{tab * tab_stops}#{tab }", "[#{child.total}] #{child.file.split(File::SEPARATOR).last + ' ' if child.file}(#{child.name})")
+            end
+          end
+        end
 
       end
     end
